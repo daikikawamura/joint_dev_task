dad86559c97904ea4c5feaac39b53e8244970d81
daiki kawamura <daguihecun@gmail.com>
2021-02-07 15:48:19 +0900

    Q7.Q9の回答

--- task.rb
+++ task.rb
@@ -65,6 +65,8 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
+
 
   # 以下は変更しないで下さい
   p array
@@ -74,9 +76,12 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map {|item| item.capitalize}
+  programming_languages = upper_case_programming_languages.map {|item| item.upcase}
+
 
   # 以下は変更しないで下さい
-  p programming_languages
+  p programming_languages 
   p upper_case_programming_languages
 end
 
@@ -84,6 +89,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
